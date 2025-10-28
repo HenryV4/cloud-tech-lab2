594cb6577ff40fd9d55b3c5a7e81f279b1039c53
HenryV4 <150185495+HenryV4@users.noreply.github.com>
2025-10-28 21:21:04 +0200

    Adjust wait time and update task definitions

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,7 @@
 from locust import HttpUser, task, between
-
 class MyRestUser(HttpUser):
-    # Тепер затримка може бути меншою, 
-    # оскільки сам запит "важкий"
-    wait_time = between(0.5, 1) 
+    wait_time = between(1, 3) # Даємо йому час
     host = "https://hotel-api-lab2-851473349647.europe-west3.run.app"
-
     @task
-    def load_cpu_endpoint(self):
-        """ Створюємо РЕАЛЬНЕ навантаження на CPU """
-        self.client.get("/cpu_load")
-
-    # @task(1)
-    # def get_root(self): ... (видаляємо старі завдання)
+    def get_root(self):
+        self.client.get("/")
